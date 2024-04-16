b803b0b5147871e0a1648837588c678d37b0ea62
Bojana Popov <bojanapopov@Bojanas-MacBook-Pro.local>
2024-04-16 16:05:50 +0200

    SN-6 Add login logic

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,9 +2,8 @@ import axios from 'src/config/axios/axios';
 import AuthenticationRequest from 'src/models/AuthenticationRequest';
 import NewUser from 'src/models/NewUser';
 
-export const authenticate = (authenticationRequest: AuthenticationRequest) => {
-  // TODO: API call
-  console.log(authenticationRequest);
+export const authenticate = async (authenticationRequest: AuthenticationRequest) => {
+  return await axios.post(`/auth/login`, authenticationRequest);
 };
 
 export const register = async (newUser: NewUser) => {
